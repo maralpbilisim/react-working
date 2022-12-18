3997cfca7c43b25e20c025c37dce54462336ffe6
maralpbilisim <72076365+maralpbilisim@users.noreply.github.com>
2022-12-18 15:57:03 +0300

    components added

--- react-first-app/src/App.js
+++ react-first-app/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './components/Header';
 
+const name="Mehmet";
+const islog=true;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <>
+    //   <h1>hello</h1>
+    // <Header></Header>
+    // </>
+   <React.Fragment>
+    <h1 className='xyz'>{islog ? `benim adım ${name}`:"kullanıcı bulunamadı"}</h1>
+    {/* {!islog && ""}<br/> */}
+    {/* <label htmlFor="myinput">aaa</label>
+    <input type="text" id='myinput'></input> */}
+  {/* <Header></Header> */}
+  </React.Fragment>
   );
 }
 
